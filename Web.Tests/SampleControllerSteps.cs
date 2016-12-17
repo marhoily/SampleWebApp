848f06cf402b79a279cdca9f921640148b3b1463
marhoily <marhoily@homtail.com>
2016-12-17 14:31:00 +0300

    again

---
+++ Web.Tests/SampleControllerSteps.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TechTalk.SpecFlow;
+
+namespace Web.Tests
+{
+    [Binding]
+    public sealed class SampleControllerSteps
+    {
+        private readonly SelfhostedServerPageObject _server = new SelfhostedServerPageObject();
+        private Task<string> _response;
+
+        [Given("POST request to (.*)")]
+        public void HttpPost(string uri, string args)
+        {
+            _response = _server.Get(uri);
+        }
+        [When("GET request to (.*)")]
+        public void HttpGet(string uri)
+        {
+            _response = _server.Get(uri);
+        }
+
+        [Then("the response should be")]
+        public void ResponseShouldBe(string expected)
+        {
+            var a = JToken.Parse(_response.Result).ToString(Formatting.Indented);
+            var e = JToken.Parse(expected).ToString(Formatting.Indented);
+            a.Should().Be(e);
+        }
+    }
+}
